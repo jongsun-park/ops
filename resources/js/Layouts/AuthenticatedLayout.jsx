f22261ae81ab5265522433a23fc7e51bc6d5d96a
Jongsun <jongsun250@gmail.com>
2023-10-19 21:20:15 +0100

    page transition added

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
 import ApplicationLogo from "@/Components/ApplicationLogo";
 import Dropdown from "@/Components/Dropdown";
 import NavLink from "@/Components/NavLink";
 import ResponsiveNavLink from "@/Components/ResponsiveNavLink";
 import { Link, usePage } from "@inertiajs/react";
+import { useState } from "react";
 
-export default function Authenticated({ user, header, children }) {
+export default function Authenticated({ user, header, children, className }) {
   const [showingNavigationDropdown, setShowingNavigationDropdown] =
     useState(false);
 
@@ -25,12 +25,12 @@ export default function Authenticated({ user, header, children }) {
   const isAdmin = usePage().props.auth.user.isAdmin || false;
 
   return (
-    <div className="min-h-screen bg-gray-100">
-      <nav className="bg-white border-b border-gray-100">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex justify-between h-16">
+    <div className={`min-h-screen bg-gray-100 ${className}`}>
+      <nav className="border-b border-gray-100 bg-white">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="flex h-16 justify-between">
             <div className="flex">
-              <div className="shrink-0 flex items-center">
+              <div className="flex shrink-0 items-center">
                 <Link href="/">
                   <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
                 </Link>
@@ -61,19 +61,19 @@ export default function Authenticated({ user, header, children }) {
               </div>
             </div>
 
-            <div className="hidden sm:flex sm:items-center sm:ml-6">
-              <div className="ml-3 relative">
+            <div className="hidden sm:ml-6 sm:flex sm:items-center">
+              <div className="relative ml-3">
                 <Dropdown>
                   <Dropdown.Trigger>
                     <span className="inline-flex rounded-md">
                       <button
                         type="button"
-                        className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                        className="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none"
                       >
                         {user.name}
 
                         <svg
-                          className="ml-2 -mr-0.5 h-4 w-4"
+                          className="-mr-0.5 ml-2 h-4 w-4"
                           xmlns="http://www.w3.org/2000/svg"
                           viewBox="0 0 20 20"
                           fill="currentColor"
@@ -108,10 +108,10 @@ export default function Authenticated({ user, header, children }) {
               <button
                 onClick={() =>
                   setShowingNavigationDropdown(
-                    (previousState) => !previousState
+                    (previousState) => !previousState,
                   )
                 }
-                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+                className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 transition duration-150 ease-in-out hover:bg-gray-100 hover:text-gray-500 focus:bg-gray-100 focus:text-gray-500 focus:outline-none"
               >
                 <svg
                   className="h-6 w-6"
@@ -148,7 +148,7 @@ export default function Authenticated({ user, header, children }) {
             (showingNavigationDropdown ? "block" : "hidden") + " sm:hidden"
           }
         >
-          <div className="pt-2 pb-3 space-y-1">
+          <div className="space-y-1 pb-3 pt-2">
             {routes.map(({ url, name }) => (
               <ResponsiveNavLink
                 key={url}
@@ -171,12 +171,12 @@ export default function Authenticated({ user, header, children }) {
               ))}
           </div>
 
-          <div className="pt-4 pb-1 border-t border-gray-200">
+          <div className="border-t border-gray-200 pb-1 pt-4">
             <div className="px-4">
-              <div className="font-medium text-base text-gray-800">
+              <div className="text-base font-medium text-gray-800">
                 {user.name}
               </div>
-              <div className="font-medium text-sm text-gray-500">
+              <div className="text-sm font-medium text-gray-500">
                 {user.email}
               </div>
             </div>
--- resources/js/Layouts/Layoput.jsx
+++ resources/js/Layouts/Layoput.jsx
@@ -1,18 +1,39 @@
-import { usePage } from "@inertiajs/react";
+import { router, usePage } from "@inertiajs/react";
 
+import { useEffect, useMemo, useState } from "react";
 import AuthenticatedLayout from "./AuthenticatedLayout";
-import GuestLayout from "./GuestLayout";
 import Flash from "./Flash";
+import GuestLayout from "./GuestLayout";
 
 const Layout = ({ children }) => {
   const user = usePage().props.auth.user;
 
+  //   Page Transition
+  const [transitioning, setTransitioning] = useState(null);
+
+  useEffect(() => {
+    router.on("start", () => {
+      setTransitioning(true);
+    });
+    router.on("finish", () => {
+      setTransitioning(false);
+    });
+  }, []);
+
+  const pageTransition = useMemo(
+    () =>
+      `animate__animated ${
+        transitioning ? "animate__fadeOut" : "animate__fadeIn"
+      }`,
+    [transitioning],
+  );
+
   if (!user) {
-    return <GuestLayout>{children}</GuestLayout>;
+    return <GuestLayout className={pageTransition}>{children}</GuestLayout>;
   }
 
   return (
-    <AuthenticatedLayout user={user}>
+    <AuthenticatedLayout user={user} className={pageTransition}>
       {children}
       <Flash />
     </AuthenticatedLayout>
