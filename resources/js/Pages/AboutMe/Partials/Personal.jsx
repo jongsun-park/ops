1c592c6905e9495e1f9ebb1bbb96200fcce6533f
Jongsun <jongsun250@gmail.com>
2023-10-24 23:27:34 +0100

    tab text color bug fix - styled overrided

--- resources/js/Pages/AboutMe/Partials/Personal.jsx
+++ resources/js/Pages/AboutMe/Partials/Personal.jsx
@@ -91,10 +91,10 @@ const Personal = () => {
               key={category}
               className={({ selected }) =>
                 classNames(
-                  "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700",
+                  "w-full rounded-lg py-2.5 text-sm font-medium leading-5",
                   "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
                   selected
-                    ? "bg-white font-semibold shadow"
+                    ? "bg-white font-semibold text-blue-700 shadow"
                     : "text-blue-100 hover:bg-white/[0.12] hover:text-white",
                 )
               }
