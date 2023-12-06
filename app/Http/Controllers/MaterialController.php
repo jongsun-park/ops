a685838cabc4c260fdeeaf0ecd2b3434d2a9c4d8
Jongsun <jongsun250@gmail.com>
2023-12-06 19:49:43 +0000

    material controller updated

--- app/Http/Controllers/MaterialController.php
+++ app/Http/Controllers/MaterialController.php
@@ -16,4 +16,35 @@ class MaterialController extends Controller
             "materials" => Material::all(),
         ]);
     }
+
+    public function store(Request $request)
+    {
+
+        $attributes = $request->validate([
+            'name' => 'required|max:255|unique:materials,name',
+            'code' => 'required|max:255',
+        ]);
+
+        $id = Material::create($attributes)->id;
+
+        return to_route('materials.index')->with('message', "Wash Option Created");
+    }
+
+    public function update(Request $request, Material $material)
+    {
+        $attributes = $request->validate([
+            'name' => 'required|max:255', // TODO: Unique validation
+            'code' => 'required|max:255',
+        ]);
+
+        $material->update($attributes);
+
+        return back()->with('message', "Wash Option Updated");
+    }
+
+    public function destroy(Material $material)
+    {
+        $material->delete();
+        return to_route('materials.index')->with('message', "Wash Option Deleted");
+    }
 }
--- app/Models/Material.php
+++ app/Models/Material.php
@@ -12,6 +12,7 @@ class Material extends Model
     public $timestamps = false;
 
     protected $fillable = [
-        'name'
+        'name',
+        'code'
     ];
 }
